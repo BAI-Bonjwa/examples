ac62f5d2eb4f7ec32df86b92d3a03ced6c0698c0
BAI-Bonjwa <baibonjwa@gmail.com>
2020-02-12 15:43:53 +0800

    Add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ const App = () => {
     <div className="App">
       <nav className="nav">
         <h1 className="header brightgreen">Examples</h1>
+
+        <ul className="links">
+          <li><a href="/webgl">WebGL</a></li>
+          <li><a href="/css">CSS</a></li>
+          <li><a href="/games">Games</a></li>
+        </ul>
+
       </nav>
     </div>
   );
